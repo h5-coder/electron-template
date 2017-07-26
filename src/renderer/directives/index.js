a4c7a7f40eae2115e7228e41ef917c90e1a62fc9
liangyanxiang <liangyanxiang@juzix.io>
2017-07-26 16:29:29 +0800

    修改模版

---
+++ src/renderer/directives/index.js
@@ -0,0 +1,4 @@
+
+//import lineChartDir from './modules/line-chart-dir.js'
+
+export default [/*lineChartDir*/]
---
+++ src/renderer/router/map/Index.js
@@ -0,0 +1,7 @@
+const LandingPage = resolve => require(['@/views/landing-page/landing-page'], resolve)
+
+export default {
+    path: '/',
+    name: 'landing-page',
+    component: LandingPage
+}
