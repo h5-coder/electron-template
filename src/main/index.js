40e24a88dac6b6acb934ed3ff13b0a40408dc685
liangyanxiang <liangyanxiang@juzix.io>
2017-07-28 14:50:50 +0800

    增加最小宽高限制逻辑

--- src/main/index.js
+++ src/main/index.js
@@ -18,13 +18,15 @@ const winURL = process.env.NODE_ENV === 'development' ?
 function createWindow() {
     // 创建浏览器窗口。
     mainWindow = new BrowserWindow({
-        height: 768,
-        useContentSize: true,
         width: 1200,
-    })
+        height: 768,
+        minWidth:1200,
+        minHeight:600,
+        useContentSize: true,
+    });
 
     // 加载应用的 index.html。
-    mainWindow.loadURL(winURL)
+    mainWindow.loadURL(winURL);
 
     // 打开开发者工具。
     //mainWindow.webContents.openDevTools();
